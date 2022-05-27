09c308a962b7687bd1bd7cbc9df329558aa9544d
behzad.b.i.g@gmail.com <behza@DESKTOP-62KEJTU>
2022-05-27 17:40:10 +0430

    Same File Sote Action Default State

--- src/App.js
+++ src/App.js
@@ -12,16 +12,44 @@ function App() {
   }
 
   //setup reducder
-  function reducer(state,action){
-    console.log({action,state})
-    return state;
+  function reducer(state = defaultState ,action){
+    switch (action.type) {
+      case "DECREASE":
+        return {...state , count : state.count -1};
+      case "INCREASE":
+        return {...state, count : state.count  + 1}
+      case "RESET":
+        return {...state, count : 0} 
+
+      default:
+        return state;
+    }
+    
+    // console.log({action,state})
+    // if (action.type === "DECREASE") {
+    //     // state.count = state.count - 1;
+    //     return {...state , count : state.count -1}
+
+    // }else if (action.type === "INCREASE"){
+    //   return {...state , count : state.count + 1}
+    // }
+    // return state;
   }
 
 
   //steup store 
-  const store = createStore(reducer,defaultState); 
+  const store = createStore(reducer); 
+  
+
+  store.dispatch({ type: "DECREASE"})
+  
+  store.dispatch({ type: "INCREASE"})
+
+  
+  store.dispatch({ type: "RESET"})
   
   console.log(store.getState(),'store');
+  
   return (
     <div>
         <Counter state={store.getState()}/>
