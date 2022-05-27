e74432712eaf4f84700e305c6e8f1b57c05a9ca2
behzad.b.i.g@gmail.com <behza@DESKTOP-62KEJTU>
2022-05-27 17:53:01 +0430

    Seperated REDUX Files

--- src/App.js
+++ src/App.js
@@ -2,51 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter'; 
 import {createStore, configureStore} from 'redux';
+import reducer from './reducer'
+import { DECREASE,INCREASE,RESET } from './actions';
+
+
+
+
+//setup reducder
+const defaultState = {
+  count : 76,
+  name : 'bob'
+}
 
 
 function App() {
 
-  const defaultState = {
-    count : 76,
-    name : 'bob'
-  }
-
-  //setup reducder
-  function reducer(state = defaultState ,action){
-    switch (action.type) {
-      case "DECREASE":
-        return {...state , count : state.count -1};
-      case "INCREASE":
-        return {...state, count : state.count  + 1}
-      case "RESET":
-        return {...state, count : 0} 
-
-      default:
-        return state;
-    }
-    
-    // console.log({action,state})
-    // if (action.type === "DECREASE") {
-    //     // state.count = state.count - 1;
-    //     return {...state , count : state.count -1}
-
-    // }else if (action.type === "INCREASE"){
-    //   return {...state , count : state.count + 1}
-    // }
-    // return state;
-  }
 
 
   //steup store 
-  const store = createStore(reducer); 
+  const store = createStore(reducer,defaultState); 
   
 
-  store.dispatch({ type: "DECREASE"})
+  store.dispatch({ type: DECREASE})
   
-  store.dispatch({ type: "INCREASE"})
+  store.dispatch({ type: INCREASE})
 
   
-  store.dispatch({ type: "RESET"})
+  store.dispatch({ type: RESET})
   
   console.log(store.getState(),'store');
   
