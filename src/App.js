a57f327db078bee71f843dd0a0d2989c0ce13e1a
behzad.b.i.g@gmail.com <behza@DESKTOP-62KEJTU>
2022-05-27 15:50:48 +0430

    Counter Component Created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
+import { useState } from 'react';
 
 function App() {
+  const [count,setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className='container'>
+            <h1>
+              Counter
+            </h1>
+            <p className='counter'>{count}</p>
+            <div className="counter">
+              <button type='button' className="btn" onClick={() => setCount(count -1)}>
+                decrease
+              </button>
+              <button type='button' className="btn" onClick={() => setCount(0)}>
+                Reset
+              </button>
+              <button type='button' className="btn" onClick={() => setCount(count +1)}>
+                Increase
+              </button>
+            </div>
+        
+        </div>
+    
+        <Counter/>
     </div>
   );
 }
---
+++ src/Counter.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Counter(){
+    return <h1>Counter Component</h1>
+}
+
+
+export default Counter;
